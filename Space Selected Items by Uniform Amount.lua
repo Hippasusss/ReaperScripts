e64f8c44a4094a70511c2bcc9b0e4863bf5f63e4
gjttt1 <dannyherbertgj@gmail.com>
2025-06-30 17:06:14 +0100

    fixed error

--- Space Selected Items by Uniform Amount.lua
+++ Space Selected Items by Uniform Amount.lua
@@ -6,8 +6,8 @@ local function main()
     if CountMediaItems < 1 then goto continue end
     local nextPosition = reaper.GetMediaItemInfo_Value(reaper.GetSelectedMediaItem(0, 0), "D_POSITION")
     for j = 0, CountMediaItems-1 do
-        local currentItem = reaper.GetSelectedMediaItem(0,j);
-        local reaper.SetMediaItemPosition(currentItem, nextPosition, true)
+        local currentItem = reaper.GetSelectedMediaItem(0,j)
+        reaper.SetMediaItemPosition(currentItem, nextPosition, true)
         nextPosition = reaper.GetMediaItemInfo_Value(currentItem, "D_POSITION") + reaper.GetMediaItemInfo_Value(currentItem, "D_LENGTH") + distanceBetweenItems
     end
     ::continue::
